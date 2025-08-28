39dd77af20ad8f18ce189159786ba6f3dd22d101
kizunofxo09 <kizunofxo09@gmail.com>
2025-08-27 22:51:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "#3c3c3c"
+
+let img = document.createElement("img");
+img.src = "https://upload.wikimedia.org/wikipedia/commons/5/57/Fr%C3%BChling_bl%C3%BChender_Kirschenbaum.jpg";
+img.alt = "texto :D";
+document.body.appendChild(img);
