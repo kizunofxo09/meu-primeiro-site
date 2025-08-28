4c701a79c27374702aa17c6022ab1199c1e78106
kizunofxo09 <kizunofxo09@gmail.com>
2025-08-27 23:16:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-document.body.style.backgroundColor = "#3c3c3c"
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.backgroundColor = "#3c3c3c";
 
-let img = document.createElement("img");
-img.src = "https://upload.wikimedia.org/wikipedia/commons/5/57/Fr%C3%BChling_bl%C3%BChender_Kirschenbaum.jpg";
-img.alt = "texto :D";
-document.body.appendChild(img);
+    let img = document.createElement("img");
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/5/57/Fr%C3%BChling_bl%C3%BChender_Kirschenbaum.jpg";
+    img.alt = "texto :D";
+    document.body.appendChild(img);
+});
