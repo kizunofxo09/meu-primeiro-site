db2c037ebaf2a573fa0c94a6e4f7fefb9ff7a6b2
kizunofxo09 <kizunofxo09@gmail.com>
2025-08-27 23:42:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.body.style.backgroundColor = "#3c3c3c";
+    
+    let img = document.createElement("img");
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/5/57/Fr%C3%BChling_bl%C3%BChender_Kirschenbaum.jpg";
+    img.alt = "texto :D";
+    document.body.appendChild(img);
+    
+
+    document.body.style.zoom = "0.85";
+    document.body.style.transform = "scale(0.85)";
+    document.body.style.transformOrigin = "0 0";
+});
